69986fb2930c4ea50428e50e1e5df8cf2896b0ad
SergioPerm <itotdel@list.ru>
2020-05-21 13:03:20 +0500

    first commit

---
+++ objC_CoreData/objC_CoreData/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  objC_CoreData
+//
+//  Created by kluv on 21/05/2020.
+//  Copyright © 2020 com.kluv.hw24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ objC_CoreData/objC_CoreData/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  objC_CoreData
+//
+//  Created by kluv on 21/05/2020.
+//  Copyright © 2020 com.kluv.hw24. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
